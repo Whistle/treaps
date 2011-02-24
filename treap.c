f50ac4a52186a89a78de37e832c9dad05d37114d
Dominic Prass <dominic.prass@googlemail.com>
2011-02-24 19:50:25 +0100

    *e to *n conversion

--- treap.c
+++ treap.c
@@ -5,169 +5,169 @@
 int null_count=0;
 
 /* Hilffunktionen */
-int is_leaf(struct treap_node *e) {
-	if(e==0)
+int is_leaf(struct treap_node *n) {
+	if(n==0)
 		return -1;
-	if(e->left || e->right)
+	if(n->left || n->right)
 		return 0;
 	return 1;
 }
 
-int is_left_child(struct treap_node *e) {
-	if(e==0 || e->parent==0)
+int is_left_child(struct treap_node *n) {
+	if(n==0 || n->parent==0)
 		return -1;
-	if(e == e->parent->left)
+	if(n == n->parent->left)
 		return 1;
 	else
 		return 0;
 }
 
 void print_smallest(struct treap *t) {
-	struct treap_node *e=0;
+	struct treap_node *n=0;
 	if(t==0)
 		return;
-	e=t->root;
-	while(e->left) {
-		e=e->left;
+	n=t->root;
+	while(n->left) {
+		n=n->left;
 	}
-	printf("Smallest number:\nValue: %d Priority: %d\n", e->value, e->priority);
+	printf("Smallest number:\nValue: %d Priority: %d\n", n->value, n->priority);
 }
 
 void print_biggest(struct treap *t) {
-	struct treap_node *e=0;
+	struct treap_node *n=0;
 	if(t==0)
 		return;
-	e=t->root;
-	while(e->right) {
-		e=e->right;
+	n=t->root;
+	while(n->right) {
+		n=n->right;
 	}
-	printf("Biggest number:\nValue: %d Priority: %d\n", e->value, e->priority);
+	printf("Biggest number:\nValue: %d Priority: %d\n", n->value, n->priority);
 }
 
-int lrotate(struct treap *t, struct treap_node *e) {
-	struct treap_node *parent=e->parent;
-	struct treap_node *child=e->right;
+int lrotate(struct treap *t, struct treap_node *n) {
+	struct treap_node *parent=n->parent;
+	struct treap_node *child=n->right;
 	struct treap_node *two=child->left;
 
 	printf("Rotiere nach links\n");
 
 	if(parent) {
-		if(e==parent->left)
+		if(n==parent->left)
 			parent->left=child;
 		else 
 			parent->right=child;
 	} else {
 		t->root=child;
 	}   
-	child->left=e;
+	child->left=n;
 	child->parent=parent;
 
-	e->parent=child;
-	e->right=two;
+	n->parent=child;
+	n->right=two;
 	if(two) 
-		two->parent=e;
+		two->parent=n;
 	return 0;
 }
 
-int rrotate(struct treap *t, struct treap_node *e) {
-	struct treap_node *parent=e->parent;
-	struct treap_node *child=e->left;
+int rrotate(struct treap *t, struct treap_node *n) {
+	struct treap_node *parent=n->parent;
+	struct treap_node *child=n->left;
 	struct treap_node *two=child->right;
 
 	printf("Rotiere nach rechts\n");
 
 	if(parent) {
-		if(e==parent->left)
+		if(n==parent->left)
 			parent->left=child;
 		else
 			parent->right=child;
 	} else {
 		t->root=child;
 	}
-	child->right=e;
+	child->right=n;
 	child->parent=parent;
 
-	e->parent=child;
-	e->left=two;
+	n->parent=child;
+	n->left=two;
 	if(two)
-		two->parent=e;
+		two->parent=n;
 	return 0;
 }
 /* Einfache Ausgabe des Baumes */
 
-int show(struct treap_node *e) {
-	if(e==0)
+int show(struct treap_node *n) {
+	if(n==0)
 		return 0;
-	show(e->left);
-	printf("Adress: 0x%x Value: %d priorityity: %2d ParentAdr: 0x%x\n", (int)e, e->value, e->priority, (int)e->parent);
-	show(e->right);
+	show(n->left);
+	printf("Adress: 0x%x Value: %d priorityity: %2d ParentAdr: 0x%x\n", (int)n, n->value, n->priority, (int)n->parent);
+	show(n->right);
 	return 0;
 }
 
 struct treap_node *find(struct treap *t, int value) {
-	struct treap_node *e=0;
+	struct treap_node *n=0;
 	if(t==0)
 		return 0;
 	printf("Suche die %d\n", value);
-	e=t->root;
-	while(e) {
-		if(e->value==value) {
+	n=t->root;
+	while(n) {
+		if(n->value==value) {
 			printf("Gefunden\n");
-			return e;
-		} else if(e->value>value) {
-			e=e->left;
+			return n;
+		} else if(n->value>value) {
+			n=n->left;
 		} else {
-			e=e->right;
+			n=n->right;
 		}
 	}
 	return 0;
 }
 
 int delete(struct treap *t, int value) {
-	struct treap_node *e=find(t,value);
-	if(e==0)
+	struct treap_node *n=find(t,value);
+	if(n==0)
 		return 0;
 	printf("Loesche die %d\n", value);
 
 	/* Blatt werden */
-	while(is_leaf(e)==0) {
-		if(e->left && e->right) {
-			if(e->left->priority > e->right->priority) {
-				rrotate(t, e);
+	while(is_leaf(n)==0) {
+		if(n->left && n->right) {
+			if(n->left->priority > n->right->priority) {
+				rrotate(t, n);
 			} else {
-				lrotate(t, e);
+				lrotate(t, n);
 			}
-		} else if(e->left && !e->right) {
-			rrotate(t, e);
+		} else if(n->left && !n->right) {
+			rrotate(t, n);
 		} else {
-			lrotate(t, e);
+			lrotate(t, n);
 		}
 	}
 
 	/* Pointer des Vaterknotens nullen und den Knoten loeschen */
-	if(is_left_child(e)) {
-		e->parent->left=0;
-		free(e);
+	if(is_left_child(n)) {
+		n->parent->left=0;
+		free(n);
 	} else {
-		e->parent->right=0;
-		free(e);
+		n->parent->right=0;
+		free(n);
 	}
 	return 0;
 }
 
-int adjust_position(struct treap *t, struct treap_node *e) {
-	while(e->priority >= e->parent->priority) {
-		if(e->priority == e->parent->priority) {
+int adjust_position(struct treap *t, struct treap_node *n) {
+	while(n->priority >= n->parent->priority) {
+		if(n->priority == n->parent->priority) {
 			return 0;
 		}
 
-		if(is_left_child(e))
-			rrotate(t, e->parent);
+		if(is_left_child(n))
+			rrotate(t, n->parent);
 		else
-			lrotate(t, e->parent);
+			lrotate(t, n->parent);
 
 		/* Sollte nach der Rotation der neue Knoten der Root-Knoten sein ist Ende */
-		if(e->parent==0)
+		if(n->parent==0)
 			return 0;
 
 	}
@@ -175,7 +175,7 @@ int adjust_position(struct treap *t, struct treap_node *e) {
 }
 
 int insert(struct treap *t, int value, int priority) {
-	struct treap_node *e=t->root;
+	struct treap_node *n=t->root;
 	struct treap_node *new_node=(struct treap_node *)malloc(sizeof(struct treap_node));
 	if(new_node==0)
 		return -1;
@@ -189,26 +189,26 @@ int insert(struct treap *t, int value, int priority) {
 
 	/* Einfaches Einhaengen in den Baum (aufsteigend sortiert) */
 	while(1) {
-		if(e->value==value) {
+		if(n->value==value) {
 			free(new_node);
 			return 1;
 		}
-		if(e->value<value) {
-			if(e->right==0) {
-				e->right=new_node;
-				new_node->parent=e;
+		if(n->value<value) {
+			if(n->right==0) {
+				n->right=new_node;
+				new_node->parent=n;
 				break;
 			} else {
-				e=e->right;
+				n=n->right;
 			}
 		}
-		if(e->value>value) {
-			if(e->left==0) {
-				e->left=new_node;
-				new_node->parent=e;
+		if(n->value>value) {
+			if(n->left==0) {
+				n->left=new_node;
+				new_node->parent=n;
 				break;
 			} else {
-				e=e->left;
+				n=n->left;
 			}
 		}
 	}
@@ -220,31 +220,31 @@ int random_insert(struct treap *t, int value) {
 	return insert(t, value, rand()%10000);
 }
 
-int build_dottree(FILE *fd, struct treap_node *e) {
+int build_dottree(FILE *fd, struct treap_node *n) {
 	if(fd==0)
 		return -1;
-	if(e==0) {
+	if(n==0) {
 		return 0;
 	}
-	if(e->left) {
-		fprintf(fd, "%d [label=\"%d\\n%d\"]",e->value, e->priority, e->value);
-		fprintf(fd, "%d -> %d;\n", e->value, e->left->value);
-		build_dottree(fd, e->left);
+	if(n->left) {
+		fprintf(fd, "%d [label=\"%d\\n%d\"]",n->value, n->priority, n->value);
+		fprintf(fd, "%d -> %d;\n", n->value, n->left->value);
+		build_dottree(fd, n->left);
 	} else {
 		null_count++;
 		fprintf(fd, "null%d [shape=none, style=invisible];\n", null_count);
-		fprintf(fd, "%d [label=\"%d\\n%d\"]",e->value, e->priority, e->value);
-		fprintf(fd, "%d -> null%d\n", e->value, null_count);
+		fprintf(fd, "%d [label=\"%d\\n%d\"]",n->value, n->priority, n->value);
+		fprintf(fd, "%d -> null%d\n", n->value, null_count);
 	}
-	if(e->right) {
-		fprintf(fd, "%d [label=\"%d\\n%d\"]",e->value, e->priority, e->value);
-		fprintf(fd, "%d -> %d;\n", e->value,  e->right->value);
-		build_dottree(fd, e->right);
+	if(n->right) {
+		fprintf(fd, "%d [label=\"%d\\n%d\"]",n->value, n->priority, n->value);
+		fprintf(fd, "%d -> %d;\n", n->value,  n->right->value);
+		build_dottree(fd, n->right);
 	} else {
 		null_count++;
 		fprintf(fd, "null%d [shape=none, style=invisible];\n", null_count);
-		fprintf(fd, "%d [label=\"%d\\n%d\"]",e->value, e->priority, e->value);
-		fprintf(fd, "%d -> null%d;\n", e->value, null_count);
+		fprintf(fd, "%d [label=\"%d\\n%d\"]",n->value, n->priority, n->value);
+		fprintf(fd, "%d -> null%d;\n", n->value, null_count);
 	}
 	return 0;
 }
